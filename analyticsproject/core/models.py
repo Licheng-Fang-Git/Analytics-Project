b2ea56f037f41888615a578d617b3573338f7d57
Licheng Fang <andyfang1967@gmail.com>
2025-08-26 12:25:29 -0400

    creating a linkedin models

--- analyticsproject/core/models.py
+++ analyticsproject/core/models.py
@@ -4,16 +4,23 @@ from django.db import models
 
 
 class LinkedInPost(models.Model):
-    day_of_week = models.CharField(max_length=20)
-    impressions = models.IntegerField()
+    post_title = models.TextField(null=True, blank=True)
+    post_link = models.URLField(unique=True)
+    created_date = models.DateField()
+    impressions = models.IntegerField(null=True, blank=True)
     clicks = models.IntegerField(null=True, blank=True)
+    ctr = models.FloatField(null=True, blank=True)
     likes = models.IntegerField(null=True, blank=True)
     comments = models.IntegerField(null=True, blank=True)
-    shares = models.IntegerField(null=True, blank=True)
-    post_type = models.CharField(max_length=50, null=True, blank=True)
+    engagement_rate = models.FloatField(null=True, blank=True)
     category = models.CharField(max_length=100, null=True, blank=True)
     sub_category = models.CharField(max_length=100, null=True, blank=True)
-    engagement_rate = models.FloatField(null=True, blank=True)
-
+    year = models.IntegerField()
+    month = models.CharField(max_length=20)
+    day_of_week = models.CharField(max_length=20)
+    time_of_posting = models.TimeField()
+    emoji = models.BooleanField(default=False)
+    type_of_post = models.CharField(max_length=50, null=True, blank=True)
+    
     def __str__(self):
-        return f"{self.day_of_week} - {self.impressions}"
+        return f"{self.post_title or 'Untitled'}, {self.impressions}"
